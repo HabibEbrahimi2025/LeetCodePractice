b0e1882a2788c1c9c71ce9c558157bfaf91cca9e
Habib <ehabib591@gmail.com>
2025-05-26 10:25:07 +0430

    Uniqe path 2

---
+++ ArrayPractice/UniqePath2.py
@@ -0,0 +1,27 @@
+def findUniqePath(obstacleGrid):
+    def checkState(arr, i,j):
+        if i<0 or i>=len(arr) or j<0 or j>=len(arr[0]):
+            return False
+        if arr[i][j]==1:
+            return False
+        
+        return True
+    
+    def findPath(x1,y1,obstacleGrid, move,path):
+        if x1==y1 and x1==len(obstacleGrid)-1:
+            path[0]+=1
+            return
+        
+        for mm in move:
+            if checkState(obstacleGrid,x1+mm[0], y1+mm[1]):
+                obstacleGrid[x1+mm[0]][y1+mm[1]]=1
+                findPath(x1+mm[0], y1+mm[1], obstacleGrid, move, path)
+                obstacleGrid[x1+mm[0]][y1+mm[1]]=0
+    move=((0,1),(1,0))
+    path=[0]
+    findPath(0,0, obstacleGrid, move, path)
+    return path[0]
+obstacleGrid = [[0,0,0],[0,0,0],[0,0,0]]
+print(findUniqePath(obstacleGrid))
+        
+
