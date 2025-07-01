a5bf94e0f985838f0264fb20c60efb3be58098cd
Habib <ehabib591@gmail.com>
2025-07-01 13:57:51 +0430

    Find The orginal typed String

---
+++ DayStrik/FindTheOrginalTypeStr.py
@@ -0,0 +1,21 @@
+def findOrginalString(word):
+    res=1
+    i=0
+    while i<len(word):
+        count=0
+        j=i
+        while j<len(word)-1 and word[j]==word[j+1]:
+            count+=1
+            j+=1
+        if i==j:
+            i+=1
+        else:
+            i=j
+        res+=count
+    return res
+word = "aaabcd"
+print(findOrginalString(word))
+
+
+
+        
