da6b3e504bac1e3fabc9ba17f0ce9275213837be
Habib <ehabib591@gmail.com>
2025-07-28 11:25:12 +0430

    Binary Tree path version 2

--- Backtracking/BinaryTreePath.py
+++ Backtracking/BinaryTreePath.py
@@ -1,10 +1,40 @@
 # Definition for a binary tree node.
+from collections import deque
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 class Solution:
+    def __init__(self):
+        self.root=None
+
+
+
+    def setValue(self, node):
+        if not node:
+            return
+        
+        self.root=TreeNode(node[0])
+        que=deque([self.root])
+
+        i=1
+        while que:
+            current=que.popleft()
+
+            if i<len(node) and node[i]!=None:
+                newNode = TreeNode(node[i])
+                current.left=newNode
+                que.append(current.left)
+            i+=1
+
+            if i<len(node) and node[i]!=None:
+                newNode2=TreeNode(node[i])
+                current.right=newNode2
+                que.append(current.right)
+
+            i+=1
+
     def binaryTreePaths(self, root):
         def backtracking(Node, path, allPath):
             if Node!=None:
@@ -20,3 +50,8 @@ class Solution:
         res=[]
         backtracking(root, [], res)
         return res
+
+root = [1,2,3,None,5]
+ob = Solution()
+ob.setValue(root)
+print(ob.binaryTreePaths(ob.root))
