70767fec05967c0fd27eb60b111f912404783c2b
Habib <ehabib591@gmail.com>
2025-07-30 11:28:09 +0430

    Eggs Drops With tow Eggs

---
+++ DynamicProgramming/EggsDropsWithTwoEggs.py
@@ -0,0 +1,30 @@
+'''
+You are given two identical eggs and you have access to a building with n floors labeled from 1 to n.
+
+You know that there exists a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, and any egg dropped at or below floor f will not break.
+
+In each move, you may take an unbroken egg and drop it from any floor x (where 1 <= x <= n). If the egg breaks, you can no longer use it. However, if the egg does not break, you may reuse it in future moves.
+
+Return the minimum number of moves that you need to determine with certainty what the value of f is.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: We can drop the first egg from floor 1 and the second egg from floor 2.
+If the first egg breaks, we know that f = 0.
+If the second egg breaks but the first egg didn't, we know that f = 1.
+Otherwise, if both eggs survive, we know that f = 2.
+'''
+def eggDropWithTwoEggs(n):
+    res=0
+    i=1
+    while res<n:
+        res=(i*(i+1))/2
+        i+=1
+    return i-1
+
+print(eggDropWithTwoEggs(100))
+    
--- DynamicProgramming/PartitioningArrayForMaiximumSum.py
+++ DynamicProgramming/PartitioningArrayForMaiximumSum.py
@@ -1,3 +1,16 @@
+'''
+Given an integer array arr, partition the array into (contiguous) subarrays of length at most k. After partitioning, each subarray has their values changed to become the maximum value of that subarray.
+
+Return the largest sum of the given array after partitioning. Test cases are generated so that the answer fits in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: arr = [1,15,7,9,2,5,10], k = 3
+Output: 84
+Explanation: arr becomes [15,15,15,9,10,10,10]
+'''
 def partitioningArray(arr, k):
     n=len(arr)
     dp=[0]*(n+1)
