8f79b03c528368721505f3241ed930402a075710
Habib Ebrahimi <ehabib591@gmail.com>
2025-08-25 09:48:28 +0430

    Bipartet Graph

---
+++ GraphLeetCode/BipartetGraph.py
@@ -0,0 +1,42 @@
+'''
+
+There is an undirected graph with n nodes, where each node is numbered between 0 and n - 1. You are given a 2D array graph, where graph[u] is an array of nodes that node u is adjacent to. More formally, for each v in graph[u], there is an undirected edge between node u and node v. The graph has the following properties:
+
+There are no self-edges (graph[u] does not contain u).
+There are no parallel edges (graph[u] does not contain duplicate values).
+If v is in graph[u], then u is in graph[v] (the graph is undirected).
+The graph may not be connected, meaning there may be two nodes u and v such that there is no path between them.
+A graph is bipartite if the nodes can be partitioned into two independent sets A and B such that every edge in the graph connects a node in set A and a node in set B.
+
+Return true if and only if it is bipartite.
+
+ 
+
+Example 1:
+
+
+Input: graph = [[1,2,3],[0,2],[0,1,3],[0,2]]
+Output: false
+Explanation: There is no way to partition the nodes into two independent sets such that every edge connects a node in one and a node in the other.
+'''
+def bipartiteGraph(graph):
+    n = len(graph)
+    color = [-1] * n 
+
+    def dfs(node, c):
+        color[node] = c
+        for nei in graph[node]:
+            if color[nei] == -1: 
+                if not dfs(nei, 1 - c):
+                    return False
+            elif color[nei] == c: 
+                return False
+        return True
+
+    for i in range(n):  
+        if color[i] == -1:
+            if not dfs(i, 0):
+                return False
+    return True
+
+print(bipartiteGraph([[1,2,3],[0,2],[0,1,3],[0,2]])) 
