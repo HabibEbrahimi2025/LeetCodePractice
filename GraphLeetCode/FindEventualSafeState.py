ed4b8d5075e48e4baca957cb75b5c8f5e043edc9
Habib Ebrahimi <ehabib591@gmail.com>
2025-10-16 10:01:51 +0430

    Find Eventual Safe State

---
+++ GraphLeetCode/FindEventualSafeState.py
@@ -0,0 +1,53 @@
+'''
+
+There is a directed graph of n nodes with each node labeled from 0 to n - 1. The graph is represented by a 0-indexed 2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, meaning there is an edge from node i to each node in graph[i].
+
+A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting from that node leads to a terminal node (or another safe node).
+
+Return an array containing all the safe nodes of the graph. The answer should be sorted in ascending order.
+
+ 
+
+Example 1:
+
+Illustration of graph
+Input: graph = [[1,2],[2,3],[5],[0],[5],[],[]]
+Output: [2,4,5,6]
+Explanation: The given graph is shown above.
+Nodes 5 and 6 are terminal nodes as there are no outgoing edges from either of them.
+Every path starting at nodes 2, 4, 5, and 6 all lead to either node 5 or 6.
+'''
+from collections import deque
+def findSaveState(graph):
+    n=len(graph)
+    newGraph=[[] for _ in range(n)]
+    for u in range(n):
+        for v in graph[u]:
+            newGraph[v].append(u)
+    inDegree=[0]*n
+    for u in range(n):
+        for v in newGraph[u]:
+            inDegree[v]+=1
+
+    que=deque([x for x in range(n) if inDegree[x]==0])
+
+    safeNode=[False]*n
+    while que:
+        temp=que.popleft()
+        safeNode[temp]=True
+        for neb in newGraph[temp]:
+            inDegree[neb]-=1
+            if inDegree[neb]==0:
+                que.append(neb)
+    return [i for i in range(n) if safeNode[i]]
+
+
+    
+
+
+
+graph = [[1,2],[2,3],[5],[0],[5],[],[]]
+print(findSaveState(graph))
+        
+
+
